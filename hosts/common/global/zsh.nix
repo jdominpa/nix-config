ecf806ff1fdaa0ee006db35e2ca0985008eab305
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2024-11-01 16:32:26 +0100

    Remove unneeded zsh global options

--- hosts/common/global/zsh.nix
+++ hosts/common/global/zsh.nix
@@ -6,16 +6,11 @@
     ".." = "cd ..";
   };
 
+  # Basic system wide settings for zsh
   programs.zsh = {
     enable = true;
     histSize = 10000;
     syntaxHighlighting.enable = true;
     autosuggestions.enable = true;
-    setOptions = [
-      "LIST_PACKED"             # Make completion lists more densely packed
-      "SHARE_HISTORY"           # Share history across shells
-      "HIST_IGNORE_DUPS"        # Don't add duplicate entries to history
-    ];
   };
-
 }
