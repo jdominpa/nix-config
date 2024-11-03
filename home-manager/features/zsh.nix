0140d472dfecde3112922d88c1b73a1c833526b4
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2024-11-03 12:26:11 +0100

    Improve zsh completions

--- home-manager/features/zsh.nix
+++ home-manager/features/zsh.nix
@@ -17,6 +17,28 @@
       autocd = true;
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
+      initExtra = ''
+      # Make completion:
+      # - Try exact (case-sensitive) match first.
+      # - Then fall back to case-insensitive.
+      # - Accept abbreviations after . or _ or - (ie. f.b -> foo.bar).
+      # - Substring complete (ie. bar -> foobar).
+      zstyle ':completion:*' matcher-list ''' '+m:{[:lower:]}={[:upper:]}' '+m:{[:upper:]}={[:lower:]}' '+m:{_-}={-_}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+      # Colorize completions using default `ls` colors.
+      zstyle ':completion:*' list-colors '''
+
+      # Allow completion of ..<Tab> to ../ and beyond.
+      zstyle -e ':completion:*' special-dirs '[[ $PREFIX = (../)#(..) ]] && reply=(..)'
+
+      # Categorize completion suggestions with headings:
+      zstyle ':completion:*' group-name '''
+      zstyle ':completion:*:descriptions' format %F{default}%B%{$'\e[3m'%}--- %d ---%{$'\e[23m'%}%b%f
+
+      # Enable keyboard navigation of completions in menu
+      # (not just tab/shift-tab but cursor keys as well):
+      zstyle ':completion:*' menu select
+      '';
     };
   };
 }
