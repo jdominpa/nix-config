457e6da12d381d2e81ad127f671310a4611d2d5b
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2025-03-12 20:15:39 +0100

    feat(emacs): use use-package in init.el and early-init.el

--- config/emacs/early-init.el
+++ config/emacs/early-init.el
@@ -1,22 +1,18 @@
 ;;; early-init.el --- Early Init File -*- lexical-binding: t -*-
 
-;; Resize the frame pixelwise
-(customize-set-variable 'frame-resize-pixelwise t)
-
-;; Do not resize the frame at this early stage.
-(customize-set-variable 'frame-inhibit-implied-resize t)
-
-;; Frame title
-(customize-set-variable 'frame-title-format '("%b"))
-
-;; Disable GUI elements
-(custom-set-variables '(menu-bar-mode nil)
-                      '(tool-bar-mode nil)
-                      '(scroll-bar-mode nil)
-                      '(use-dialog-box t)
-                      '(use-file-dialog nil)
-                      '(use-short-answers t)
-                      '(inhibit-startup-screen t))
+(use-package emacs
+  :custom
+  (package-enable-at-startup t)    ; Initialise installed packages
+  (frame-resize-pixelwise t)       ; Resize the frame pixelwise
+  (frame-inhibit-implied-resize t) ; Do not resize the frame at this early stage.
+  (frame-title-format '("%b"))     ; Frame title
+  ;; GUI elements
+  (tool-bar-mode nil)
+  (scroll-bar-mode nil)
+  (use-dialog-box t)
+  (use-file-dialog nil)
+  (use-short-answers t)
+  (inhibit-startup-screen t))
 
 ;; Speed up startup
 (setq gc-cons-threshold most-positive-fixnum
@@ -26,9 +22,6 @@
             (setq gc-cons-threshold (* 1024 1024 20)
                   gc-cons-percentage 0.2)
             (garbage-collect)))
-
-;; Initialise installed packages
-(customize-set-variable 'package-enable-at-startup t)
 
 (provide 'early-init)
 ;;; early-init.el ends here
--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1,22 +1,19 @@
 ;;; init.el --- Personal init file -*- lexical-binding: t -*-
 
-;; Send custom.el file to oblivion
-(customize-set-variable 'custom-file (make-temp-file "emacs-custom-"))
+(use-package emacs
+  :custom
+  (custom-file (make-temp-file "emacs-custom-")) ; Send custom.el file to oblivion
+  ;; Some basic settings
+  (initial-buffer-choice t)             ; Always start with *scratch* buffer
+  (blink-cursor-mode nil)
+  ;; Backups
+  (make-backup-files nil)
+  (create-lockfiles nil)
+  :config
+  (setq disabled-command-function nil))
 
-;; Some basic settings
-(setq disabled-command-function nil)
-(customize-set-variable 'initial-buffer-choice t)          ; always start with *scratch* buffer
-(customize-set-variable 'blink-cursor-mode nil)
-
-;; Backups
-(custom-set-variables '(make-backup-files nil)
-                      '(create-lockfiles nil))
-
-
-;;; Packages and modules
-
-;; "jdp-core" is for all my emacs configuration modules
-;; "jdp-lisp" is used for all my custom elisp files
+;; "jdp-core" is for emacs configuration modules
+;; "jdp-lisp" is used for custom elisp files
 (mapc
  (lambda (string)
    (add-to-list 'load-path (locate-user-emacs-file string)))
@@ -26,15 +23,26 @@
 (when (native-comp-available-p)
   (customize-set-variable 'native-comp-async-report-warnings-errors 'silent))
 
-(require 'package)
-(customize-set-variable 'package-archives
-                        '(("gnu-elpa" . "https://elpa.gnu.org/packages/")
-                          ("gnu-elpa-devel" . "https://elpa.gnu.org/devel/")
-                          ("melpa" . "https://melpa.org/packages/")
-                          ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
-(customize-set-variable 'package-archive-priority '(("gnu-elpa" . 3)
-                                                    ("melpa" . 2)
-                                                    ("nongnu" . 1)))
+(use-package package
+  :custom
+  (package-archives '(("gnu-elpa" . "https://elpa.gnu.org/packages/")
+                      ("gnu-elpa-devel" . "https://elpa.gnu.org/devel/")
+                      ("melpa" . "https://melpa.org/packages/")
+                      ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+  (package-archive-priorities '(("gnu-elpa" . 3)
+                                ("melpa" . 2)
+                                ("nongnu" . 1))))
+
+;; Set up `load-path' and environmental variables correctly
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (dolist (var '("SSH_AUTH_SOCK" "NIX_PATH" "NIX_PROFILES" "NIX_USER_PROFILE_DIR" "NIX_SSL_CERT_FILE"))
+    (add-to-list 'exec-path-from-shell-variables var))
+  (when (or (memq window-system '(mac ns x pgtk))
+            (unless (memq system-type '(ms-dos windows-nt))
+              (daemonp)))
+    (exec-path-from-shell-initialize)))
 
 (require 'jdp-core-meow)
 (require 'jdp-core-emacs)
@@ -49,7 +57,7 @@
 (require 'jdp-core-prog)
 
 
-;;; System settings
+;;; System specific settings
 
 (when (eq system-type 'darwin)
   (setq mac-command-modifier 'meta
