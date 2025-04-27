f6136d5dea92392e778f5db3f547a8d1476b348c
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2025-04-27 16:56:19 +0200

    feat(emacs): add lean4-mode

--- config/emacs/jdp-modules/jdp-modules-prog.el
+++ config/emacs/jdp-modules/jdp-modules-prog.el
@@ -146,6 +146,58 @@
   :ensure t
   :defer t)
 
+;; Lean 4
+(use-package lean4-mode
+  :vc (:url "https://github.com/leanprover-community/lean4-mode"
+            :rev :newest)
+  :defer t
+  :config
+  (use-package lsp-mode
+    :hook (lsp-completion-mode . jdp-lsp-mode-setup-completion)
+    :init
+    (defun jdp-lsp-mode-setup-completion ()
+      (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
+            '(emacs22 substring orderless)))
+    (defun lsp-booster--advice-json-parse (old-fn &rest args)
+      "Try to parse bytecode instead of json."
+      (or
+       (when (equal (following-char) ?#)
+         (let ((bytecode (read (current-buffer))))
+           (when (byte-code-function-p bytecode)
+             (funcall bytecode))))
+       (apply old-fn args)))
+    (advice-add (if (progn (require 'json)
+                           (fboundp 'json-parse-buffer))
+                    'json-parse-buffer
+                  'json-read)
+                :around
+                #'lsp-booster--advice-json-parse)
+    (defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+      "Prepend emacs-lsp-booster command to lsp CMD."
+      (let ((orig-result (funcall old-fn cmd test?)))
+        (if (and (not test?)                             ;; for check lsp-server-present?
+                 (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+                 lsp-use-plists
+                 (not (functionp 'json-rpc-connection))  ;; native json-rpc
+                 (executable-find "emacs-lsp-booster"))
+            (progn
+              (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
+                (setcar orig-result command-from-exec-path))
+              (message "Using emacs-lsp-booster for %s!" orig-result)
+              (cons "emacs-lsp-booster" orig-result))
+          orig-result)))
+    :custom
+    (lsp-keymap-prefix "C-c l")
+    (lsp-diagnostics-provider :flymake)
+    (lsp-completion-provider :none)
+    (lsp-log-io nil)
+    (lsp-keep-workspace-alive nil)
+    (lsp-enable-xref t)
+    (lsp-enable-symbol-highlighting nil)
+    (lsp-headerline-breadcrumb-enable nil)
+    :config
+    (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)))
+
 ;; Markdown
 (use-package markdown-mode
   :ensure t
