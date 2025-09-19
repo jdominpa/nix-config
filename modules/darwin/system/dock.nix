0b51c8f18464333f22ff9abcafbc55510b9e56dd
Joan Domingo Pasarin <jdomingopasarin@gmail.com>
2025-09-19 09:37:52 +0200

    darwin: remove launchpad from dock
    Launchpad no longer exists as of macOS Tahoe 26.0

--- modules/darwin/system/dock.nix
+++ modules/darwin/system/dock.nix
@@ -25,9 +25,6 @@ in
       mru-spaces = false;
       persistent-apps = [
         {
-          app = "/System/Applications/Launchpad.app";
-        }
-        {
           app = "/Applications/Google Chrome.app";
         }
         {
