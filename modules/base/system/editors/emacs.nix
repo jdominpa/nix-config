d5a9b64d060c3cfd4d83b455aa2a459f3cae40fd
Joan Domingo Pasarin <jdomingopasarin@gmail.com>
2025-09-24 10:38:09 +0200

    Revert 351e88f
    Native compilation on macOS has been fixed

--- modules/base/system/editors/emacs.nix
+++ modules/base/system/editors/emacs.nix
@@ -6,13 +6,7 @@
 }:
 let
   cfg = config.jdp.base.system.editors.emacs;
-  emacs =
-    # FIXME: native compilation doesn't work with macOS Sequoia 15.4
-    # https://github.com/NixOS/nixpkgs/issues/395169
-    if pkgs.stdenv.hostPlatform.isDarwin then
-      pkgs.emacs-git.override { withNativeCompilation = true; }
-    else
-      pkgs.emacs-git-pgtk;
+  emacs = if pkgs.stdenv.hostPlatform.isDarwin then pkgs.emacs-git else pkgs.emacs-git-pgtk;
 in
 {
   options.jdp.base = {
