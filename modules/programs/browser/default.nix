abf9ac3ea761996986c39520910a92f4671e9c51
Joan Domingo Pasarin <jdomingopasarin@gmail.com>
2026-02-10 10:43:00 +0100

    feat: remove google-chrome package

--- modules/programs/browser/default.nix
+++ modules/programs/browser/default.nix
@@ -3,17 +3,11 @@
   ...
 }:
 {
-  flake.modules.nixos.browser =
-    { pkgs, ... }:
-    {
-      imports = [ self.modules.nixos.zen-browser ];
-      environment.systemPackages = [ pkgs.google-chrome ];
-    };
+  flake.modules.nixos.browser = {
+    imports = [ self.modules.nixos.zen-browser ];
+  };
 
-  flake.modules.darwin.browser =
-    { pkgs, ... }:
-    {
-      imports = [ self.modules.darwin.zen-browser ];
-      environment.systemPackages = [ pkgs.google-chrome ];
-    };
+  flake.modules.darwin.browser = {
+    imports = [ self.modules.darwin.zen-browser ];
+  };
 }
