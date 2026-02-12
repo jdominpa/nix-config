14db78720f6529763db89bf7666b885116c12f1f
Joan Domingo Pasarin <jdomingopasarin@gmail.com>
2026-02-12 20:10:26 +0100

    feat: switch to brave browser

--- modules/programs/browser/default.nix
+++ modules/programs/browser/default.nix
@@ -1,13 +1,15 @@
 {
-  self,
-  ...
-}:
-{
-  flake.modules.nixos.browser = {
-    imports = [ self.modules.nixos.zen-browser ];
-  };
+  flake.modules.nixos.browser =
+    { pkgs, ... }:
+    {
+      # imports = [ self.modules.nixos.zen-browser ];
+      environment.systemPackages = [ pkgs.brave ];
+    };
 
-  flake.modules.darwin.browser = {
-    imports = [ self.modules.darwin.zen-browser ];
-  };
+  flake.modules.darwin.browser =
+    { pkgs, ... }:
+    {
+      # imports = [ self.modules.darwin.zen-browser ];
+      environment.systemPackages = [ pkgs.brave ];
+    };
 }
