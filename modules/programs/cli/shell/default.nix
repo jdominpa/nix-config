4e662694c73d4e8a4598a1c69f6734b5ea076b2e
Joan Domingo Pasarin <work@jdompas.com>
2026-04-12 10:50:14 +0200

    zsh: add edit-command-line keybind and some options

--- modules/programs/cli/shell/default.nix
+++ modules/programs/cli/shell/default.nix
@@ -74,6 +74,48 @@ in
           # Enable keyboard navigation of completions in menu
           # (not just tab/shift-tab but cursor keys as well):
           zstyle ':completion:*' menu select
+
+          # adds `cdr` command for navigating to recent directories
+          autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+          add-zsh-hook chpwd chpwd_recent_dirs
+
+          # enable menu-style completion for cdr
+          zstyle ':completion:*:*:cdr:*:*' menu selection
+
+          # fall through to cd if cdr is passed a non-recent dir as an argument
+          zstyle ':chpwd:*' recent-dirs-default true
+
+          #
+          # Options
+          #
+
+          setopt AUTO_PUSHD              # [default] cd automatically pushes old dir onto dir stack
+          setopt AUTO_RESUME             # allow simple commands to resume backgrounded jobs
+          setopt CORRECT                 # [default] command auto-correction
+          setopt CORRECT_ALL             # [default] argument auto-correction
+          setopt NO_FLOW_CONTROL         # disable start (C-s) and stop (C-q) characters
+          setopt NO_HIST_IGNORE_ALL_DUPS # don't filter non-contiguous duplicates from history
+          setopt HIST_FIND_NO_DUPS       # don't show dupes when searching
+          setopt HIST_IGNORE_DUPS        # do filter contiguous duplicates from history
+          setopt HIST_IGNORE_SPACE       # [default] don't record commands starting with a space
+          setopt HIST_VERIFY             # confirm history expansion (!$, !!, !foo)
+          setopt IGNORE_EOF              # [default] prevent accidental C-d from exiting shell
+          setopt INTERACTIVE_COMMENTS    # [default] allow comments, even in interactive shells
+          setopt LIST_PACKED             # make completion lists more densely packed
+          setopt MENU_COMPLETE           # auto-insert first possible ambiguous completion
+          setopt NO_NOMATCH              # [default] unmatched patterns are left unchanged
+          setopt PRINT_EXIT_VALUE        # [default] for non-zero exit status
+          setopt PUSHD_IGNORE_DUPS       # don't push multiple copies of same dir onto stack
+          setopt PUSHD_SILENT            # [default] don't print dir stack after pushing/popping
+          setopt SHARE_HISTORY           # share history across shells
+
+          #
+          # Bindings
+          #
+
+          autoload -U edit-command-line
+          zle -N edit-command-line
+          bindkey '^x^x' edit-command-line
         '';
       };
     };
