b8a9873e0b132c8f611abfed639f8197ea8023d5
Joan Domingo Pasarin <work@jdompas.com>
2026-07-29 12:28:29 +0200

    cleanup: remove zen browser config and flake

--- modules/programs/brave/default.nix
+++ modules/programs/brave/default.nix
@@ -2,14 +2,12 @@
   flake.modules.nixos.browser =
     { pkgs, ... }:
     {
-      # imports = [ self.modules.nixos.zen-browser ];
       environment.systemPackages = [ pkgs.brave ];
     };
 
   flake.modules.darwin.browser =
     { pkgs, ... }:
     {
-      # imports = [ self.modules.darwin.zen-browser ];
       environment.systemPackages = [ pkgs.brave ];
     };
 }
