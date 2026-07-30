jdominpa/nix-config

commit dded4c7f959be6b90182e323cafecb2954ee5a3e

refactor: remove every instance of `modules.nixos` and `modules.darwin`

modules/flake-parts/flake-parts.nix
@@ -4,7 +4,6 @@
 }:
 {
   imports = [
-    inputs.flake-parts.flakeModules.modules
     inputs.home-manager.flakeModules.home-manager
   ];
 

flake.nix
@@ -37,16 +37,8 @@
       url = "github:LnL7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-index-database = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     wrappers = {
       url = "github:BirdeeHub/nix-wrapper-modules";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    stylix = {
-      url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     treefmt-nix = {

modules/features/brave/default.nix
@@ -1,11 +1,11 @@
 {
-  flake.modules.nixos.browser =
+  flake.nixosModules.brave =
     { pkgs, ... }:
     {
       environment.systemPackages = [ pkgs.brave ];
     };
 
-  flake.modules.darwin.browser =
+  flake.darwinModules.brave =
     { pkgs, ... }:
     {
       environment.systemPackages = [ pkgs.brave ];

modules/features/cli-tools/default.nix
@@ -3,7 +3,6 @@ let
     { pkgs, ... }:
     {
       environment.systemPackages = with pkgs; [
-        bat
         btop
         coreutils
         curl
@@ -34,7 +33,7 @@ let
     };
 in
 {
-  flake.modules.nixos.cli-tools =
+  flake.nixosModules.cli-tools =
     { pkgs, ... }:
     {
       imports = [ sharedPackages ];
@@ -48,7 +47,7 @@ in
       ];
     };
 
-  flake.modules.darwin.cli-tools = {
+  flake.darwinModules.cli-tools = {
     imports = [ sharedPackages ];
   };
 }

modules/features/discord/default.nix
@@ -1,11 +1,11 @@
 {
-  flake.modules.nixos.discord =
+  flake.nixosModules.discord =
     { pkgs, ... }:
     {
       environment.systemPackages = [ pkgs.vencord ];
     };
 
-  flake.modules.darwin.discord =
+  flake.darwinModules.discord =
     { pkgs, ... }:
     {
       environment.systemPackages = [ pkgs.vencord ];

modules/features/gaming/default.nix
@@ -1,5 +1,5 @@
 {
-  flake.modules.nixos.gaming =
+  flake.nixosModules.gaming =
     { pkgs, ... }:
     {
       environment.systemPackages = [

modules/features/kanata/default.nix
@@ -1,5 +1,5 @@
 {
-  flake.modules.darwin.kanata =
+  flake.darwinModules.kanata =
     { pkgs, ... }:
     {
       environment.systemPackages = [ pkgs.kanata ];

modules/features/latex/default.nix
@@ -1,11 +1,11 @@
 {
-  flake.modules.nixos.latex =
+  flake.nixosModules.latex =
     { pkgs, ... }:
     {
       environment.systemPackages = [ pkgs.texliveFull ];
     };
 
-  flake.modules.darwin.latex =
+  flake.darwinModules.latex =
     { pkgs, ... }:
     {
       environment.systemPackages = [ pkgs.texliveFull ];

commit b1f78c55b7009681954f14364caa03ad1080ea89

fix: dded4c7 introduced a bug by removing the `modules` flake-parts import

modules/flake-parts/flake-parts.nix
@@ -4,6 +4,7 @@
 }:
 {
   imports = [
+    inputs.flake-parts.flakeModules.modules
     inputs.home-manager.flakeModules.home-manager
   ];
 